ee2bab9accd8fe8d121004b98ba1d7cc770082b9
mohammed <mo.rahman610@gmail.com>
2018-05-26 16:58:49 -0400

    first commit

---
+++ Lab18/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object o = new Object();
+
+            LinkedList list = new LinkedList();
+            list.AddAtStart("Adam");
+            list.AddAtStart("Cat");
+            list.AddAtStart("Mohammed");
+            list.AddAtStart(o);
+            list.AddAtStart("Shaun");
+            list.AddToEnd("Danny");
+            list.AddToEnd("Jaide");
+
+            list.RemoveAll("Mohammed");
+
+
+
+        }
+    }
+}
